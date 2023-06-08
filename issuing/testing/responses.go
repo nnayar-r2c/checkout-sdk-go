fcc487eb7a592c75c3639fb6c21b6841ce4a0a4a
Martin Seco <mseco.93@gmail.com>
2023-06-08 11:49:44 -0300

    Api Updates * Adds `LocalSchemes` and deprecates `SchemeLocal` in `ResponseCardSource` (CS2) * Adds support for `SimulateIncrement` for `Card Testing` [Issuing] (CS2) * Adds support for `SimulateClearing` for `Card Testing` [Issuing] (CS2) * Adds support for `SimulateReversal` for `Card Testing` [Issuing] (CS2)

--- issuing/testing/responses.go
+++ issuing/testing/responses.go
@@ -4,15 +4,23 @@ import "github.com/checkout/checkout-sdk-go/common"
 
 type TransactionStatus string
 
+type ReversalStatus string
+
 const (
 	Authorized TransactionStatus = "Authorized"
 	Declined   TransactionStatus = "Declined"
+	Reversed   TransactionStatus = "Reversed"
 )
 
 type (
 	CardAuthorizationResponse struct {
 		HttpMetadata common.HttpMetadata
 		Id           string            `json:"id,omitempty"`
+		Status       TransactionStatus `json:"status,omitempty"`
+	}
+
+	CardSimulationResponse struct {
+		HttpMetadata common.HttpMetadata
 		Status       TransactionStatus `json:"status,omitempty"`
 	}
 )
